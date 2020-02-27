6a0f855510ba8d1d04bb6e2afe06317689d18624
RashmiJayaram <rashmijayaram24@gmail.com>
2020-02-27 23:07:44 +0530

    landingPage css completed

--- fitnesspro-client/src/pages/landingPage/landingPage.js
+++ fitnesspro-client/src/pages/landingPage/landingPage.js
@@ -13,12 +13,33 @@ class LandingPage extends Component {
             <div>
                 <NavBar />
                 <div style={{ width: '100%' }}><img src={FitPic1} width='100%'></img></div>
-                <div className={'landingpage-fittext-container'}>
-                    <p><h1>MOVIE STARS</h1></p>
+                <div style={{ alignItems: 'center', width: '100%',padding:'30px' }}>
+                    <div style={{ textAlign: 'center' }}>
+                        <p style={{ display: 'inline', fontStyle: 'italic', fontSize: '35px' }}>
+                            MOVIE STARS
+                        </p>
+                    </div>
+                    <div style={{ textAlign: 'center', margin: '10px', alignSelf: 'center' }}>
+                        <p style={{ display: 'inline', fontStyle: 'bold', fontSize: '70px' }}>
+                            FITNESS IS<br />NOT SECRET
+                        </p>
+                    </div>
+                    <div style={{textAlign:'center'}}>
+                        <p>
+                            YOU ARE CELEBRITY FOR US<br/>SO TRAIN AS ONE WITH CELEB TRAINERS
+                        </p>
+                    </div>
+                </div>
+                <div style={{textAlign:'center',padding:'3px'}}>
+                    <p style={{fontStyle:'italic',fontSize:'20px'}}>
+                        Train With The Best To Become Beast
+                    </p>
                 </div>
                 <div style={{ width: '100%' }}><img src={FitPic2} width='100%'></img></div>
-                <div className={'landingpage-fittext-container'}>
-
+                <div style={{textAlign:'center',padding:'5px'}}>
+                    <p style={{fontStyle:'italic',fontSize:'20px'}}>
+                        Our Promise Is Result Not Pumpkins 
+                    </p>
                 </div>
                 <div style={{ width: '100%' }}><img src={FitPic3} width='100%'></img></div>
                 <div className={'landingpage-fittext-container'}>
