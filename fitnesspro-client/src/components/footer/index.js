90bb4b99f440cc8ba281b6d5126bf46351fdbe64
Tj9000 <tejas050197@gmail.com>
2020-03-10 00:06:48 +0530

    index change; footer to all pages; css

--- fitnesspro-client/src/components/footer/index.js
+++ fitnesspro-client/src/components/footer/index.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import './footer.css'
+import styles from './footer.module.css'
 class Footer extends Component {
     render() {
         return (
-            <div className={'footerContainer'}>
-                <div className={'footer'}>
-                    <ul className={'footerUl'}>
-                        <li><a>ABOUT FITPRO</a></li>
-                        <li><a>WHY FITPRO</a></li>
+            <div className={styles.footerContainer}>
+                <div className={styles.footer}>
+                    <ul className={styles.footerUl}>
+                        <li className={styles.footerLiHead}>ProFit</li>
+                        <li><a>ABOUT PROFIT</a></li>
+                        <li><a>WHY PROFIT</a></li>
                         <li><a>VISION AND VALUES</a></li>
                         <li><a>TRAINERS</a></li>
                     </ul>
-                    <ul className={'footerUl'}>
+                    <ul className={styles.footerUl}>
+                        <li className={styles.footerLiHead}>Legal</li>
                         <li><a>TERMS*</a></li>
                         <li><a>COPY RIGHTS</a></li>
                         <li><a>LEGAL</a></li>
                         <li><a>CONTACT US</a></li>
                     </ul>
-                    <ul className={'footerUl'}>
+                    <ul className={styles.footerUl}>
+                        <li className={styles.footerLiHead}>Social</li>
                         <li><a>FACEBOOK</a></li>
                         <li><a>INSTAGRAM</a></li>
                         <li><a>SNAPCHAT</a></li>
