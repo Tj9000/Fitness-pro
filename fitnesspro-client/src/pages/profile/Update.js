cbb9a64cd9d9b4ae73784ee1a661ae034f2a5c8e
Tj9000 <tejas050197@gmail.com>
2020-03-12 21:35:59 +0530

    some ui fixes

--- fitnesspro-client/src/pages/profile/Update.js
+++ fitnesspro-client/src/pages/profile/Update.js
@@ -34,7 +34,7 @@ class Update extends Component {
             this.setState({
                 newImageFile: file,
                 newImageBase64: reader.result
-            }, ()=>{ console.log(this.state)});
+            }, () => { console.log(this.state) });
         }
         reader.readAsDataURL(file)
     }
@@ -48,22 +48,23 @@ class Update extends Component {
 
     render() {
         let InputListData = [
-            { label: 'Name', type: 'text', defaultValue: this.props.userDetails && this.props.userDetails.name },
+            { label: 'Name', type: 'text', readOnly: true, defaultValue: this.props.userDetails && this.props.userDetails.name },
             { label: 'Age', type: 'number', defaultValue: this.props.userDetails && this.props.userDetails.age },
             { label: 'Gender', type: 'radio', options: ['Male', 'Female', 'Other'], defaultValue: this.props.userDetails && this.props.userDetails.gender },
             { label: 'Height', type: 'number', defaultValue: this.props.userDetails && this.props.userDetails.height },
             { label: 'Weight', type: 'number', defaultValue: this.props.userDetails && this.props.userDetails.weight },
-            { label: 'Email', type: 'email', defaultValue: this.props.userDetails && this.props.userDetails.email },
-            { label: 'Phone', type: 'text', defaultValue: this.props.userDetails && this.props.userDetails.phone },
+            { label: 'Email', type: 'email', readOnly: true, defaultValue: this.props.userDetails && this.props.userDetails.email },
+            { label: 'Phone', type: 'text', readOnly: true, defaultValue: this.props.userDetails && this.props.userDetails.phone },
+            { label: 'Country', type: 'text', readOnly: true, defaultValue: this.props.userDetails && this.props.userDetails.country },
             { label: 'City', type: 'text', defaultValue: this.props.userDetails && this.props.userDetails.city },
             { label: 'Zipcode', type: 'number', defaultValue: this.props.userDetails && this.props.userDetails.zipCode },
             { label: 'Gym Access', type: 'text', defaultValue: this.props.userDetails && this.props.userDetails.gymAccess },
             { label: 'Target', type: 'text', defaultValue: this.props.userDetails && this.props.userDetails.target },
-            { label: 'Language Preference', type: 'text', defaultValue: this.props.userDetails && this.props.userDetails.languagePref }
+            { label: 'Language Preference', type: 'text', readOnly: true, defaultValue: this.props.userDetails && this.props.userDetails.languagePref }
         ];
         return (<div className={styles.profileContentContainer}>
             <div className={styles.profileContentSubContainer}>
-                <div profileContentLeftList>
+                <div className={styles.profileContentLeftList}>
                     <ListInput inputs={InputListData}
                         ref={ref => this.formref = ref}
                     />
