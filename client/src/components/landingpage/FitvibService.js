3d756f2ab4731ba671c63a4fa9009fb8e16996ab
RashmiJayaram <rashmijayaram24@gmail.com>
2020-03-18 23:37:02 +0530

    landing page css

---
+++ client/src/components/landingpage/FitvibService.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './fitvibservice.module.css'
+import Gym from '../../assets/images/gym.png'
+
+class FitvibService extends React.Component {
+    render() {
+        return (
+            <div className={styles.serviceContainer}>
+                <div className={styles.imageWrapper}>
+                    <img src={Gym} height='70px' width='70px' className={styles.image}></img>
+                </div>
+                <div className={styles.serviceHeader}>{this.props.serviceHeader}</div>
+                <div className={styles.serviceContent}>
+                    {/* <p style={{ textAlign: 'center' }}> */}
+                        {this.props.serviceDesc}
+                    {/* </p> */}
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default FitvibService;
--- client/src/pages/landingPage/landingPage.js
+++ client/src/pages/landingPage/landingPage.js
@@ -3,19 +3,58 @@ import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
 
 import LandingNavBar from '../../components/navBar/Landing';
-import Footer from '../../components/footer';
-import FitPic1 from '../../assets/images/fitPic1.png'
+import FitPic1 from '../../assets/images/landingpage/bg_2.jpg'
+import FitPic4 from '../../assets/images/landingpage/ripped-man.jpg'
+
 import FitPic2 from '../../assets/images/fitPic2.png'
 import FitPic3 from '../../assets/images/fitPic3.png'
-import './landingPage.css'
+import styles from './landingPage.module.css'
+import FitvibService from "../../components/landingpage/FitvibService";
+
 
 class LandingPage extends Component {
     render() {
         return (
             <div className="pageMainContainer">
                 <LandingNavBar />
-                <div style={{ width: '100%' }}><img src={FitPic1} width='100%'></img></div>
-                <div style={{ width: '100%' }}>
+                <div className={styles.imgC1} style={{ width: '100%' }}>
+                    <div className={[styles.imgC1_def, styles.imgC1_1].join(' ')}>
+                        {/* <img className={styles.bottom} src={FitPic4} width='100%'></img> */}
+                        <div className={styles.textContainer}>
+                            <div className={styles.imageTextheader}>Challenge <span style={{ color: '#e5ce48' }}>yourself</span></div>
+                            <div className={styles.imageTextContent}>Train to be a better version of yourself, everyday!</div>
+                            <div className={styles.buttonWrapper}><button className={styles.joinNow}>Join now</button></div>
+                        </div>
+                    </div>
+                    <div className={[styles.imgC1_def, styles.imgC1_2].join(' ')}>
+                        {/* <img className={styles.top} src={FitPic1} width='100%'></img> */}
+                        <div className={styles.textContainer}>
+                            <div className={styles.imageTextheader}>We are <span style={{ color: '#e5ce48' }}>FitVib</span></div>
+                            <div className={styles.imageTextContent}> The future of fitness</div>
+                            <div className={styles.buttonWrapper}><button className={styles.joinNow}>Join now</button></div>
+                        </div>
+                    </div>
+                </div>
+                <div>                    
+                    <div className={styles.serviceHeading}>Trust the <span style={{color:'#e5ce48'}}>process</span></div>
+                    <div className={styles.serviceSubHeading}>Some motivational lines here</div>
+                    <div className={styles.serviceContainer}>
+                        <div className={styles.fitvibServiceContainer}>
+                            <FitvibService serviceHeader="Analyse your goal" serviceDesc="Random content 1" />
+                        </div>
+                        <div className={styles.fitvibServiceContainer}>
+                            <FitvibService serviceHeader="work hard on it" serviceDesc="Random content 2." />
+                        </div>
+                        <div className={styles.fitvibServiceContainer}>
+                            <FitvibService serviceHeader="Improve your performance" serviceDesc="Random content 3." />
+                        </div>
+                        <div className={styles.fitvibServiceContainer}>
+                            <FitvibService serviceHeader="Achieve perfect body" serviceDesc="Random content 4." />
+                        </div>
+                    </div>
+                </div>
+
+                {/* <div style={{ width: '100%' }}>
                     <div style={{ textAlign: 'center' }}>
                         <p style={{ display: 'inline', fontStyle: 'italic', fontSize: '35px' }}>
                             MOVIE STARS
@@ -46,10 +85,8 @@ class LandingPage extends Component {
                 <div style={{ width: '100%' }}><img src={FitPic3} width='100%'></img></div>
                 <div className={'landingpage-fittext-container'}>
 
-                </div>
+                </div> */}
             </div>
-
-
         )
     }
 
