eaa6bc14989772c18fb4b31da9df2d4c5b8609fe
RashmiJayaram <rashmijayaram24@gmail.com>
2020-03-20 00:51:06 +0530

    homepage css mvp

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -1,13 +1,20 @@
 import React from "react";
-import styles from './homepageCard.module.css'
-import Muscle from '../../../assets/images/gym.png'
+import styles from './card.module.css'
+import Muscle from '../../assets/images/gym.png'
 
-
-export default class HomePageCard extends React.Component {
+class Card extends React.Component {
     render() {
         return (
             <div className={styles.nutritionCard}>
-                <div className={styles.nutritionCardDesc}>Building Muscle
+                <div className={styles.nutritionCardDesc}>Building Muscle</div>
+                <div className={styles.nutritionCardList}>
+                    <p>
+                    Breakfast : Egg-white omelet with chicken and veggies<br/><br/>
+                    Lunch : 1-2 grilled chicken breasts 2 handful-sized servings of brown rice<br/><br/>
+                    Dinner : Large salad with mixed veggies 1 baked regular or sweet potato<br/><br/>
+                    </p>
+                </div>
+                {/* <div className={styles.nutritionCardDesc}>Building Muscle
                         <img src={Muscle} height='20px' width='50px' style={{ paddingLeft: '10px' }}></img></div>
                 <ul style={{ listStyleType: 'none', padding: '10px' }}>
                     <li className={styles.nutritionCardList}>Breakfast :
@@ -18,8 +25,10 @@ export default class HomePageCard extends React.Component {
                             </li>
                     <li className={styles.nutritionCardList}>Dinner : Large salad with mixed veggies
                             1 baked regular or sweet potato</li>
-                </ul>
+                </ul> */}
             </div>
         )
     }
 }
+
+export default Card;
